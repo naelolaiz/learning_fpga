20b06778e047289b06f0beb82b2e93cda955a232
naelolaiz <1333555+naelolaiz@users.noreply.github.com>
2022-06-29 23:58:03 +0200

    second try: using button as xor for second led

--- blink_led.vhd
+++ blink_led.vhd
@@ -3,13 +3,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Led_blink is
 	Port (clk : in STD_LOGIC;
-              led : out STD_LOGIC);
+	      button1 : in STD_LOGIC;
+			led : out STD_LOGIC;
+			led2 : out STD_LOGIC);
 end Led_blink;
 
 architecture Behavioral of Led_blink is
 	signal pulse : std_LOGIC := '0';
 	signal count : integer range 0 to 50000000 := 0;
 begin
+
+	
 	counter : process(clk)
 	begin
 		if clk'event and clk = '1' then
@@ -23,5 +27,7 @@ begin
 	
 	end process;
 	
-        led <= pulse;
+	led <= pulse;
+	led2 <= pulse xnor button1;
+	
 end Behavioral;
