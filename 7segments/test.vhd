31e5ee2682627341fe4a864fda63b056fb3247f5
naelolaiz <1333555+naelolaiz@users.noreply.github.com>
2022-06-30 03:28:39 +0200

    using clock with counter instead of buttons for display input (0-9)

--- 7segments/test.vhd
+++ 7segments/test.vhd
@@ -4,7 +4,8 @@ USE ieee.std_logic_1164.ALL;
 
 entity test is
    port (
-	      inputButtons : in std_logic_vector(3 downto 0);
+         clock : in std_logic;
+         inputButtons : in std_logic_vector(3 downto 0);
          sevenSegments : out std_logic_vector(6 downto 0);
          cableSelectDigit0 : out std_logic;
          cableSelectDigit1 : out std_logic;
@@ -13,26 +14,42 @@ entity test is
 end test;
 
 architecture behavior of test is
+signal counterForDelay  : integer range 0 to 10000000 := 0; -- ticks every 10E6 / 50E6 = 200ms 
+signal counterToDisplay : integer range 0 to 9 := 0; -- TODO: multiplex cableSelectDigits -- 0xFFFF := 0;
+
 begin
    cableSelectDigit0 <= '0';
    cableSelectDigit1 <= '0';
    cableSelectDigit2 <= '0';
    cableSelectDigit3 <= '0';
-   sevenSegments <= "1000000" when inputButtons=not "0000" else
-        "1111001" when inputButtons=not "0001" else
-        "0100100" when inputButtons=not "0010" else
-        "0110000" when inputButtons=not "0011" else
-        "0011001" when inputButtons=not "0100" else
-        "0010010" when inputButtons=not "0101" else
-        "0000010" when inputButtons=not "0110" else
-        "1111000" when inputButtons=not "0111" else
-        "0000000" when inputButtons=not "1000" else
-        "0010000" when inputButtons=not "1001" else
-        "0001000" when inputButtons=not "1010" else
-        "0000011" when inputButtons=not "1011" else
-        "1000110" when inputButtons=not "1100" else
-        "0100001" when inputButtons=not "1101" else
-        "0000110" when inputButtons=not "1110" else
-        "0001110" ;
+   
+   counter : process(clock)
+   begin
+      if clock'event and clock = '1' then
+         if counterForDelay = 9999999 then
+            counterForDelay <= 0;
+            counterToDisplay <= counterToDisplay + 1;
+         else
+            counterForDelay <= counterForDelay + 1;
+         end if;
+      end if;
 
+   end process;
+
+   sevenSegments <= "1000000" when counterToDisplay = 0 else
+        "1111001" when counterToDisplay =  1 else
+        "0100100" when counterToDisplay =  2 else
+        "0110000" when counterToDisplay =  3 else
+        "0011001" when counterToDisplay =  4 else
+        "0010010" when counterToDisplay =  5 else
+        "0000010" when counterToDisplay =  6 else
+        "1111000" when counterToDisplay =  7 else
+        "0000000" when counterToDisplay =  8 else
+        "0010000" when counterToDisplay =  9 else
+        "0001000" when counterToDisplay = 10 else
+        "0000011" when counterToDisplay = 11 else
+        "1000110" when counterToDisplay = 12 else
+        "0100001" when counterToDisplay = 13 else
+        "0000110" when counterToDisplay = 14 else
+        "0001110" ;
 end behavior;
