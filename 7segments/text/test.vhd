dc47e98c090f363e3cf1b5e8359c47d6ecd8e6f8
nael <naelblog@gmail.com>
2022-07-03 22:48:42 +0200

    updated text for example

--- 7segments/text/test.vhd
+++ 7segments/text/test.vhd
@@ -2,7 +2,6 @@ library ieee;
 
 use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.ALL;
---use ieee.std_logic_unsigned.all;
 
 entity test is
    port (
@@ -13,17 +12,17 @@ entity test is
 end test;
 
 architecture behavior of test is
-	signal counterForDelay  : integer range 0 to 15000000 := 0; -- ticks every 10E6 / 50E6 = 200ms 
+	signal counterForDelay  : integer range 0 to 8000000 := 0; -- ticks every 10E6 / 50E6 = 200ms 
 	signal enabledDigit: std_logic_vector (1 downto 0) := "00";
 	signal counterForMultiplexer : integer range 0 to 100000 := 0;
 	signal LED_BCD: std_logic_vector (3 downto 0);
 	signal charForDigit: character := nul;
-	constant stringToPrint: string := " - HELLO - hello - THIS IS A TEST - not enough chars -";
+	constant stringToPrint: string := "_-+-_- Hello FPGA Wworld _-+-==- ";
 	signal stringOffset: integer range 0 to stringToPrint'length-1:= 0;
 begin
 
+-- counter for multiplexer and delay used for scrolling the text
    counter : process(clock)
-
    begin
       if clock'event and clock = '1' then
 		   if counterForMultiplexer = 99999 then
@@ -32,7 +31,7 @@ begin
 		   else
 			   counterForMultiplexer <= counterforMultiplexer + 1;
 			end if;
-         if counterForDelay = 14999999 then
+         if counterForDelay = 7999999 then
             counterForDelay <= 0;
 				if stringOffset = stringToPrint'length-1 then
 				   stringOffset <= 0;
@@ -53,7 +52,7 @@ begin
 		-- and http://atlas.physics.arizona.edu/~kjohns/downloads/vhdl/VHDL-xilinx-help.pdf : Foundation Express Packages -> std_logic_arith Package -> Conversion Functions : rol
 		cableSelect <= not std_logic_vector(unsigned(InputForShifter) rol (to_integer(unsigned(enabledDigit))));
 		
-      -- select char to display according to the current mux active output and the stringOffset
+      -- select char to display according to the current enabled digit and the stringOffset
 		charForDigit <= stringToPrint(((stringOffset + to_integer(unsigned(not(enabledDigit)))) mod stringToPrint'length)+1);
 	end process;
 	
@@ -71,14 +70,20 @@ begin
 			when '7' => sevenSegments <= "11111000";
 			when '8' => sevenSegments <= "10000000";
 			when '9' => sevenSegments <= "10010000";
+			when '=' => sevenSegments <= "11110110";
+			when '+' => sevenSegments <= "11111110";
 			when '-' => sevenSegments <= "10111111";
+			when '_' => sevenSegments <= "11110111";
 			when ' ' => sevenSegments <= "11111111";
+			when ''' => sevenSegments <= "11111101";
+			when ',' => sevenSegments <= "11111011";
+			when '.' => sevenSegments <= "01111111";
 			when 'A' => sevenSegments <= "10001000";
 			when 'a' => sevenSegments <= "00100000";
 			when 'B' => sevenSegments <= "10000011";
 			when 'b' => sevenSegments <= "10000011";
 			when 'C' => sevenSegments <= "11000110";
-			when 'c' => sevenSegments <= "11000110";
+			when 'c' => sevenSegments <= "10100111";
 			when 'D' => sevenSegments <= "10100001";
 			when 'd' => sevenSegments <= "10100001";
 			when 'E' => sevenSegments <= "10000110";
@@ -95,6 +100,8 @@ begin
 			when 'j' => sevenSegments <= "11110001";
 			when 'L' => sevenSegments <= "11000111";
 			when 'l' => sevenSegments <= "11001111";
+			when 'M' => sevenSegments <= "11001100"; --first part of a multidigit M
+			when 'm' => sevenSegments <= "11011000"; --second part of a multidigit M
 			when 'N' => sevenSegments <= "10101011";
 			when 'n' => sevenSegments <= "10101011";
 			when 'O' => sevenSegments <= "11000000"; 
@@ -111,24 +118,15 @@ begin
 			when 't' => sevenSegments <= "10001111";
 			when 'U' => sevenSegments <= "11000001";
 			when 'u' => sevenSegments <= "11100011";
+			when 'W' => sevenSegments <= "11000011"; --first part of multidigit W
+			when 'w' => sevenSegments <= "11100001"; --second part of multidigit W
+			when 'X' => sevenSegments <= "11110000"; --first part of multidigit X
+			when 'x' => sevenSegments <= "11000110"; --second part of multidigit X
+			when 'Y' => sevenSegments <= "10011011"; --first part of multidigit y
+			when 'y' => sevenSegments <= "10101101"; --second part of multidigit y
+			when 'Z' => sevenSegments <= "10100100";
+			when 'z' => sevenSegments <= "10100100";
 			when others => sevenSegments <= "11111111";
 		end case;
 	end process;
---   sevenSegments <= "0100011" when LED_BCD = "0000" else
---	--"1000000" when LED_BCD = "0000" else
---        "1111001" when LED_BCD =  "0001" else
---        "0100100" when LED_BCD =  "0010" else
---        "0110000" when LED_BCD =  "0011" else
---        "0011001" when LED_BCD =  "0100" else
---        "0010010" when LED_BCD =  "0101" else
---        "0000010" when LED_BCD =  "0110" else
---        "1111000" when LED_BCD =  "0111" else
---        "0000000" when LED_BCD =  "1000" else
---        "0010000" when LED_BCD =  "1001" else
---        "0001000" when LED_BCD =  "1010" else
---        "0000011" when LED_BCD =  "1011" else
---        "1000110" when LED_BCD =  "1100" else
---        "0100001" when LED_BCD =  "1101" else
---        "0000110" when LED_BCD =  "1110" else
---        "0001110" ;
 end behavior;
