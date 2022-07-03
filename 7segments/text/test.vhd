f5003d2cb89dfb441a73062453a1b888e8753442
nael <naelblog@gmail.com>
2022-07-03 16:27:38 +0200

    using dot for lower case "a". showing hiHI instead of HiHI

--- 7segments/text/test.vhd
+++ 7segments/text/test.vhd
@@ -8,7 +8,7 @@ entity test is
    port (
          clock : in std_logic;
          inputButtons : in std_logic_vector(3 downto 0);
-         sevenSegments : out std_logic_vector(6 downto 0);
+         sevenSegments : out std_logic_vector(7 downto 0);
          cableSelect : out std_logic_vector(3 downto 0));
 end test;
 
@@ -20,7 +20,7 @@ architecture behavior of test is
 	signal LED_BCD: std_logic_vector (3 downto 0);
 	signal charForDigit: character := nul;
 	signal charsToPrint: string (1 to 4);
-	constant stringToPrint: string := "HiHI";
+	constant stringToPrint: string := "hiHI";
 	
 begin
 
@@ -70,52 +70,52 @@ begin
 	process(charForDigit)
 	begin
 		case charForDigit is
-			when '0' => sevenSegments <= "1000000";
-			when '1' => sevenSegments <= "1111001";
-			when '2' => sevenSegments <= "0100100";
-			when '3' => sevenSegments <= "0110000";
-			when '4' => sevenSegments <= "0011001";
-			when '5' => sevenSegments <= "0010010";
-			when '6' => sevenSegments <= "0000010";
-			when '7' => sevenSegments <= "1111000";
-			when '8' => sevenSegments <= "0000000";
-			when '9' => sevenSegments <= "0010000";
-			when ' ' => sevenSegments <= "1111111";
-			when 'A' => sevenSegments <= "0001000";
-			when 'a' => sevenSegments <= "0001000";
-			when 'B' => sevenSegments <= "0000011";
-			when 'b' => sevenSegments <= "0000011";
-			when 'C' => sevenSegments <= "1000110";
-			when 'c' => sevenSegments <= "1000110";
-			when 'D' => sevenSegments <= "0100001";
-			when 'd' => sevenSegments <= "0100001";
-			when 'E' => sevenSegments <= "0000110";
-			when 'e' => sevenSegments <= "0000100";
-			when 'F' => sevenSegments <= "0001110";
-			when 'f' => sevenSegments <= "0001110";
-			when 'G' => sevenSegments <= "0010000";
-			when 'g' => sevenSegments <= "0010000";
-			when 'H' => sevenSegments <= "0001001";
-			when 'h' => sevenSegments <= "0001011";
-			when 'I' => sevenSegments <= "1001111";
-			when 'i' => sevenSegments <= "1101111";
-			when 'J' => sevenSegments <= "1110001";
-			when 'j' => sevenSegments <= "1110001";
-			when 'L' => sevenSegments <= "1000111";
-			when 'l' => sevenSegments <= "1000111";
-			when 'N' => sevenSegments <= "0101011";
-			when 'n' => sevenSegments <= "0101011";
-			when 'O' => sevenSegments <= "1000000"; 
-			when 'o' => sevenSegments <= "0100011";
-			when 'P' => sevenSegments <= "0001100";
-			when 'p' => sevenSegments <= "0001100";
-			when 'R' => sevenSegments <= "0101111";
-			when 'r' => sevenSegments <= "0101111";
-			when 'S' => sevenSegments <= "0010010";
-			when 's' => sevenSegments <= "0010010";
-			when 'U' => sevenSegments <= "1000001";
-			when 'u' => sevenSegments <= "1100011";
-			when others => sevenSegments <= "1111111";
+			when '0' => sevenSegments <= "11000000";
+			when '1' => sevenSegments <= "11111001";
+			when '2' => sevenSegments <= "10100100";
+			when '3' => sevenSegments <= "10110000";
+			when '4' => sevenSegments <= "10011001";
+			when '5' => sevenSegments <= "10010010";
+			when '6' => sevenSegments <= "10000010";
+			when '7' => sevenSegments <= "11111000";
+			when '8' => sevenSegments <= "10000000";
+			when '9' => sevenSegments <= "10010000";
+			when ' ' => sevenSegments <= "11111111";
+			when 'A' => sevenSegments <= "10001000";
+			when 'a' => sevenSegments <= "00100000";
+			when 'B' => sevenSegments <= "10000011";
+			when 'b' => sevenSegments <= "10000011";
+			when 'C' => sevenSegments <= "11000110";
+			when 'c' => sevenSegments <= "11000110";
+			when 'D' => sevenSegments <= "10100001";
+			when 'd' => sevenSegments <= "10100001";
+			when 'E' => sevenSegments <= "10000110";
+			when 'e' => sevenSegments <= "10000100";
+			when 'F' => sevenSegments <= "10001110";
+			when 'f' => sevenSegments <= "10001110";
+			when 'G' => sevenSegments <= "10010000";
+			when 'g' => sevenSegments <= "10010000";
+			when 'H' => sevenSegments <= "10001001";
+			when 'h' => sevenSegments <= "10001011";
+			when 'I' => sevenSegments <= "11001111";
+			when 'i' => sevenSegments <= "11101111";
+			when 'J' => sevenSegments <= "11110001";
+			when 'j' => sevenSegments <= "11110001";
+			when 'L' => sevenSegments <= "11000111";
+			when 'l' => sevenSegments <= "11001111";
+			when 'N' => sevenSegments <= "10101011";
+			when 'n' => sevenSegments <= "10101011";
+			when 'O' => sevenSegments <= "11000000"; 
+			when 'o' => sevenSegments <= "10100011";
+			when 'P' => sevenSegments <= "10001100";
+			when 'p' => sevenSegments <= "10001100";
+			when 'R' => sevenSegments <= "10101111";
+			when 'r' => sevenSegments <= "10101111";
+			when 'S' => sevenSegments <= "10010010";
+			when 's' => sevenSegments <= "10010010";
+			when 'U' => sevenSegments <= "11000001";
+			when 'u' => sevenSegments <= "11100011";
+			when others => sevenSegments <= "11111111";
 		end case;
 	end process;
 --   sevenSegments <= "0100011" when LED_BCD = "0000" else
