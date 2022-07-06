f42307fc3e3678d8dd2207df7fdd57e34d1defe7
nael <naelblog@gmail.com>
2022-07-06 02:22:26 +0200

    fixed indentation

--- 7segments/clock/7segmentsDigit.vhd
+++ 7segments/clock/7segmentsDigit.vhd
@@ -8,8 +8,8 @@ use ieee.numeric_std.ALL;
 entity Digit is
    generic (MAX_NUMBER: integer range 0 to 9 := 9);
    port (clockForIncrement : in std_logic := '0';
-	      reset : in std_logic := '0';
-			--set: in std_logic := 0; -- when 1, clock is use to increment the currentNumber 
+         reset : in std_logic := '0';
+         --set: in std_logic := 0; -- when 1, clock is use to increment the currentNumber 
          currentNumber : out std_logic_vector (3 downto 0) := "0000";
          carryBit : out std_logic := '0');
 end Digit;
@@ -19,8 +19,8 @@ begin
     increment: process(clockForIncrement, reset)
     begin
        if clockForIncrement'event and clockForIncrement = '1' then
-		    --if set = '1' then		    
-			 --end if;
+          --if set = '1' then        
+          --end if;
           if currentNumberSignal = MAX_NUMBER then
              currentNumberSignal <= 0;
              carryBit <= '1';
@@ -29,9 +29,9 @@ begin
              carryBit <= '0';
           end if;
        end if;
-		 if reset = '1' then
-		    currentNumberSignal <= 0;
-		 end if;
+       if reset = '1' then
+          currentNumberSignal <= 0;
+       end if;
     end process;
     currentNumber <= std_logic_vector(to_unsigned(currentNumberSignal, 4));
 end behaviorDigit;
