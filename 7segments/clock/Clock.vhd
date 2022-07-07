d0989cb2c8935a310eab748bb98efb7e96b892c7
naelolaiz <1333555+naelolaiz@users.noreply.github.com>
2022-07-07 20:42:50 +0200

    big refactoring: using a single counter for clock. Deleted cascaded 7 segment digits

--- 7segments/clock/Clock.vhd
+++ 7segments/clock/Clock.vhd
@@ -15,54 +15,28 @@ entity Clock is
 end Clock;
 
 architecture behaviorClock of Clock is
-signal carryBitSecondsUnit, carryBitSecondsTens, carryBitMinutesUnit, carryBitMinutesTens, carryBitHoursUnit: std_logic := '0';
-signal decimalCounterHours: std_logic_vector(4 downto 0) := std_logic_vector(to_unsigned(0,5));
+constant SECONDS_PER_DAY : std_logic_vector (16 downto 0) := std_logic_vector(to_unsigned(86400,17));
+signal decimalCounterSeconds: std_logic_vector(16 downto 0) := std_logic_vector(to_unsigned(0,17)); 
+signal decimalHours: integer range 0 to 23 := 0;
+signal decimalMinutes: integer range 0 to 59 := 0;
+signal decimalSeconds: integer range 0 to 59 := 0;
+
 begin
- -- digit instances ...               
-   digitSecsUnit : entity work.Digit(behaviorDigit)
-    generic map (MAX_NUMBER => 9)
-    port map (
-      clock => clock,
-      direction => direction,
-      currentNumber => bcdDigits(3 downto 0),
-      carryBit => carryBitSecondsUnit,
-      reset => reset);
-
-      digitSecsTens : entity work.Digit(behaviorDigit)
-    generic map (MAX_NUMBER => 5)
-    port map (
-      clock => carryBitSecondsUnit,
-      direction => direction,
-      currentNumber => bcdDigits(7 downto 4),
-      carryBit => carryBitSecondsTens,
-      reset => reset); 
-      
-   digitMinsUnit : entity work.Digit(behaviorDigit)
-    generic map (MAX_NUMBER => 9)
-    port map (
-      clock => carryBitSecondsTens,
-      direction => direction,
-      currentNumber => bcdDigits(11 downto 8),
-      carryBit => carryBitMinutesUnit,
-      reset => reset); 
-      
-   digitMinsTens : entity work.Digit(behaviorDigit)
-    generic map (MAX_NUMBER => 5)
-    port map (
-      clock => carryBitMinutesUnit,
-      direction => direction,
-      currentNumber => bcdDigits(15 downto 12),
-      carryBit => carryBitMinutesTens,
-      reset => reset); 
-   ------------------------------------------------------------------
-   -- counter for for multiplexer (4 digits, one increment every 2ms)                
-   digitsHours : entity work.Counter(behaviorCounterForDigit)
-    generic map (MAX_NUMBER_FOR_COUNTER => 23)
-    port map ( clock => carryBitMinutesTens,
+   daySecondsCounter : entity work.Counter(behaviorCounterForDigit)
+    generic map (MAX_NUMBER_FOR_COUNTER => std_logic_vector(to_unsigned(0,47)) & SECONDS_PER_DAY)
+    port map ( clock => clock,
                direction => direction,
-	       counter(4 downto 0) => decimalCounterHours,
-	       reset => reset);
-   bcdDigits(19 downto 16) <= std_logic_vector(to_unsigned(to_integer(unsigned(decimalCounterHours)) mod 10, 4));
-   bcdDigits(23 downto 20) <= std_logic_vector(to_unsigned(to_integer(unsigned(decimalCounterHours)) / 10, 4));
-
+               counter(16 downto 0) => decimalCounterSeconds,
+               reset => reset);
+   -- get hours, minutes and seconds in decimal
+   decimalHours <= to_integer(unsigned(decimalCounterSeconds)) / 3600;
+   decimalMinutes <= (to_integer(unsigned(decimalCounterSeconds)) mod 3600) / 60;
+   decimalSeconds <= (to_integer(unsigned(decimalCounterSeconds)) mod 3600) mod 60;
+   -- decimal to bcd
+   bcdDigits(3 downto 0) <=   std_logic_vector(to_unsigned(decimalSeconds mod 10, 4)); -- seconds unit
+   bcdDigits(7 downto 4) <=   std_logic_vector(to_unsigned(decimalSeconds / 10, 4));   -- seconds tens
+   bcdDigits(11 downto 8) <=  std_logic_vector(to_unsigned(decimalMinutes mod 10, 4)); -- minutes unit
+   bcdDigits(15 downto 12) <= std_logic_vector(to_unsigned(decimalMinutes / 10, 4));   -- minutes tens
+   bcdDigits(19 downto 16) <= std_logic_vector(to_unsigned(decimalHours mod 10, 4));   -- hour unit digits
+   bcdDigits(23 downto 20) <= std_logic_vector(to_unsigned(decimalHours / 10, 4));     -- hour tens
 end behaviorClock;
--- 7segments/clock/CounterTimer.vhd
+++ 7segments/clock/CounterTimer.vhd
@@ -9,7 +9,6 @@ entity CounterTimer is
             MAX_NUMBER_FOR_COUNTER: integer := 10);
    port (clock: in std_logic := '0';
          reset: in std_logic := '0';
-	 direction: in std_logic := '1'; -- 1 = forward
          timerTriggered : out std_logic := '0';
          counter : out std_logic_vector (63 downto 0):= std_logic_vector(to_unsigned(0,64)));
 end CounterTimer;
@@ -26,20 +25,11 @@ begin
    counterProcess : process(timerTick, reset)
    begin
       if timerTick'event and timerTick = '1' then
-         -- TODO : simplify?
-	 if direction = '1' then
-            if counterValue = std_logic_vector(to_unsigned(MAX_NUMBER_FOR_COUNTER, counterValue'length)) then
-               counterValue <= std_logic_vector(to_unsigned(0, counterValue'length));
-            else
-               counterValue <= std_logic_vector(to_unsigned(to_integer(unsigned(counterValue))+1, counterValue'length));
-            end if;
-	 else -- going backwards
-            if counterValue = std_logic_vector(to_unsigned(0, counterValue'length)) then
-               counterValue <= std_logic_vector(to_unsigned(MAX_NUMBER_FOR_COUNTER,counterValue'length));
-            else
-               counterValue <= std_logic_vector(to_unsigned(to_integer(unsigned(counterValue))-1, counterValue'length));
-            end if;
-	 end if;
+        if counterValue = std_logic_vector(to_unsigned(MAX_NUMBER_FOR_COUNTER, counterValue'length)) then
+           counterValue <= std_logic_vector(to_unsigned(0, counterValue'length));
+        else
+           counterValue <= std_logic_vector(to_unsigned(to_integer(unsigned(counterValue))+1, counterValue'length));
+        end if;
       end if;
       if reset = '1' then
           counterValue <= std_logic_vector(to_unsigned(0,64));
@@ -48,46 +38,3 @@ begin
    timerTriggered <= TimerTick;
    counter <= counterValue;
 end behaviorCounterTimer;
-
-LIBRARY ieee;
-use ieee.std_logic_1164.ALL;
-use ieee.numeric_std.ALL;
-entity Counter is
-   generic (MAX_NUMBER_FOR_COUNTER: integer := 10);
-   port (clock: in std_logic := '0';
-         reset: in std_logic := '0';
-	 direction: in std_logic := '1'; -- 1 = forward
-         timerTriggered : out std_logic := '0';
-         counter : out std_logic_vector (63 downto 0):= std_logic_vector(to_unsigned(0,64)));
-end Counter;
-architecture behaviorCounterForDigit of Counter is
-   signal counterValue : std_logic_vector (63 downto 0) := std_logic_vector(to_unsigned(0,64));
-begin
-   counterProcess : process(clock, reset)
-   begin
-      if clock'event and clock = '1' then
-         -- TODO : simplify?
-	 if direction = '1' then
-            if counterValue = std_logic_vector(to_unsigned(MAX_NUMBER_FOR_COUNTER, 64)) then --counterValue'length)) then
-               counterValue <= std_logic_vector(to_unsigned(0, 64));
-	       timerTriggered <= '1';
-            else
-               counterValue <= std_logic_vector(to_unsigned(to_integer(unsigned(counterValue))+1, 64));
-	       timerTriggered <= '0';
-            end if;
-	 else -- going backwards
-            if counterValue = std_logic_vector(to_unsigned(0, 64)) then
-               counterValue <= std_logic_vector(to_unsigned(MAX_NUMBER_FOR_COUNTER, 64));
-	       timerTriggered <= '1';
-            else
-               counterValue <= std_logic_vector(to_unsigned(to_integer(unsigned(counterValue))-1, 64));
-	       timerTriggered <= '0';
-            end if;
-	 end if;
-      end if;
-      if reset = '1' then
-          counterValue <= std_logic_vector(to_unsigned(0,64));
-      end if;
-   end process;
-   counter <= counterValue;
-end behaviorCounterForDigit;
