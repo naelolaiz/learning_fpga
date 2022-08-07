680816915432d8acd6637e9ef8feb28673e5e424
naelolaiz <1333555+naelolaiz@users.noreply.github.com>
2022-08-08 00:21:16 +0200

    testing blink led

--- blink_led/blink_led.vhd
+++ blink_led/blink_led.vhd
@@ -1,23 +1,24 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity blink_led is
-   Port (clk : in STD_LOGIC;
-         button1 : in STD_LOGIC;
-         led : out STD_LOGIC;
-         led2 : out STD_LOGIC);
+   generic (CLOCKS_TO_OVERFLOW : integer := 50E6);
+   port (clk     : in std_logic;
+         button1 : in std_logic;
+         led     : out std_logic;
+         led2    : out std_logic);
 end blink_led;
 
 architecture Behavioral of blink_led is
    signal pulse : std_LOGIC := '0';
-   signal count : integer range 0 to 50000000 := 0;
+   signal count : integer range 0 to CLOCKS_TO_OVERFLOW := 0;
 begin
 
    
    counter : process(clk)
    begin
       if clk'event and clk = '1' then
-         if count = 49999999 then
+         if count = CLOCKS_TO_OVERFLOW-1 then
             count <= 0;
             pulse <= not pulse;
          else
