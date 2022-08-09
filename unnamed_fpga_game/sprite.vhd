9ce9abdf47a70ab1308654863496dffd272e55bb
nael <naelblog@gmail.com>
2022-08-09 15:05:35 +0200

    copied unnamed game from external repository

---
+++ unnamed_fpga_game/sprite.vhd
@@ -0,0 +1,173 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.ALL;
+
+library ieee_proposed;
+use ieee_proposed.fixed_pkg.all;
+
+library work;
+use work.definitions.all;
+
+entity sprite is
+   generic (SCREEN_SIZE    : Size2D := (800,600);
+            SPRITE_WIDTH   : integer := 7;
+            SCALE          : integer := 3;
+            SPRITE_CONTENT : std_logic_vector := "1001001"
+                                               & "0101010"
+                                               & "0011100"
+                                               & "1111111"
+                                               & "0011100"
+                                               & "0101010"
+                                               & "1001001";
+            INITIAL_ROTATION         : AngleType := (others=>'0');
+            INITIAL_ROTATION_SPEED           : RotationSpeed := ( 1, 0);
+            INITIAL_POSITION         : Pos2D   := (0, 0);
+            INITIAL_SPEED            : Speed2D := (0, 0, 0)
+            );
+   port( inClock : in  std_logic;
+         inEnabled : in boolean;
+         --inSpritePos   : Pos2D; -- center position of sprite
+         inCursorPos   : Pos2D; -- position to check
+         --inRotation    : in AngleType;
+         inColision : in boolean; --TODO direction
+         outShouldDraw : out boolean);
+end;
+
+architecture logic of sprite is
+   constant SPRITE_SIZE          : Size2D := (SPRITE_WIDTH, SPRITE_CONTENT'length / SPRITE_WIDTH);
+   type SPRITE_CONTENT_TYPE is array (SPRITE_SIZE.height-1 downto 0) of std_logic_vector (SPRITE_SIZE.width-1 downto 0);
+
+   signal sSpriteContent  : SPRITE_CONTENT_TYPE;
+   signal sCenterPos : Pos2D := (0,0);
+   constant C_HALF_SCALED_WIDTH  : integer := SPRITE_SIZE.width * SCALE / 2;
+   constant C_HALF_SCALED_HEIGHT : integer := SPRITE_SIZE.height * SCALE / 2;
+
+   signal sRotation : AngleType := INITIAL_ROTATION;
+   signal sSpritePos : Pos2D := (INITIAL_POSITION.x,
+                                  INITIAL_POSITION.y);
+   signal sCurrentSpeed : Speed2D := INITIAL_SPEED;
+   signal sCurrentRotationSpeed : RotationSpeed := INITIAL_ROTATION_SPEED;
+begin
+
+   rotateSprite : process  (inClock)
+      variable counterForSpriteRotationUpdate : integer := 0;
+      variable indexForSpriteRotation : integer range 0 to TRIGONOMETRIC_FUNCTIONS_TABLE'LENGTH-1;
+   begin
+       if rising_edge(inClock) then
+          if counterForSpriteRotationUpdate = sCurrentRotationSpeed.update_period then
+             counterForSpriteRotationUpdate := 0;
+             if sCurrentRotationSpeed.index_inc > 0 then
+                 if indexForSpriteRotation = indexForSpriteRotation'HIGH then
+                    indexForSpriteRotation := 0;
+                 else
+                    indexForSpriteRotation := indexForSpriteRotation + sCurrentRotationSpeed.index_inc;
+                 end if;
+             elsif sCurrentRotationSpeed.index_inc < 0 then
+                 if indexForSpriteRotation = 0 then
+                    indexForSpriteRotation := indexForSpriteRotation'HIGH;
+                 else
+                    indexForSpriteRotation := indexForSpriteRotation + sCurrentRotationSpeed.index_inc;
+                 end if;
+             end if;
+             sRotation <= TRIGONOMETRIC_FUNCTIONS_TABLE(indexForSpriteRotation).angle;
+          else
+             counterForSpriteRotationUpdate := counterForSpriteRotationUpdate + 1;
+          end if;
+   
+       end if;
+   end process;
+
+   moveSprite : process (inClock)
+      variable counterForSpritePositionUpdate : integer range 0 to INITIAL_SPEED.update_period := 0;
+      variable nextPositionToTest : Pos2D := (0,0);
+      variable collisionDetected : boolean := false;
+   begin
+      if rising_edge(inClock) then
+         if counterForSpritePositionUpdate = counterForSpritePositionUpdate'HIGH then
+            counterForSpritePositionUpdate := 0;
+            collisionDetected := false;
+           -- check for colission with the screen
+            -- TODO: implement vectors sum
+            nextPositionToTest := ((sSpritePos.x + sCurrentSpeed.x),
+                                  (sSpritePos.y + sCurrentSpeed.y));
+            if  nextPositionToTest.x - C_HALF_SCALED_WIDTH <= 0
+             or nextPositionToTest.x + C_HALF_SCALED_WIDTH >= SCREEN_SIZE.width then
+               sCurrentSpeed.x <= sCurrentSpeed.x * (-1);
+               collisionDetected := true;
+            end if;
+            if  nextPositionToTest.y - C_HALF_SCALED_HEIGHT <= 0
+             or nextPositionToTest.y + C_HALF_SCALED_HEIGHT >= SCREEN_SIZE.height then
+               sCurrentSpeed.y <= sCurrentSpeed.y * (-1);
+               collisionDetected := true;
+            end if;
+            sSpritePos <= ((sSpritePos.x + sCurrentSpeed.x),
+                           (sSpritePos.y + sCurrentSpeed.y));
+            if collisionDetected or (inColision and outShouldDraw) then
+               sCurrentRotationSpeed.index_inc <= sCurrentRotationSpeed.index_inc * (-1); 
+            end if;
+            if (inColision and outShouldDraw) then
+               sCurrentSpeed.x <= sCurrentSpeed.x * (-1);
+               sCurrentSpeed.y <= sCurrentSpeed.y * (-1);
+            end if;
+
+         else
+            counterForSpritePositionUpdate := counterForSpritePositionUpdate + 1;
+         end if;
+      end if;
+   end process;
+
+
+  RefreshsSpriteContent : process (inClock)
+    variable oneDimensionalPointer: integer := 0;
+  begin
+    -- TODO : assert proper height and width
+    for i in SPRITE_SIZE.height-1 downto 0 loop
+       oneDimensionalPointer := i*SPRITE_WIDTH;
+       for o in SPRITE_SIZE.width-1 downto 0 loop
+          sSpriteContent(i)(o) <= SPRITE_CONTENT(oneDimensionalPointer+o);
+       end loop;
+    end loop;
+  end process;
+
+  ProcessPosition : process(inClock,
+                            sSpritePos,
+                            inCursorPos,
+                            inEnabled)
+    variable vCursor : Pos2D := (0, 0);
+    variable vTranslatedCursor: Pos2D := (0, 0);
+  begin
+      if not inEnabled then
+          outShouldDraw <= false;
+      elsif rising_edge(inClock) then
+          sCenterPos <= sSpritePos;
+
+          vCursor := inCursorPos;
+
+          if   vCursor.x < (sCenterPos.x - C_HALF_SCALED_WIDTH)
+            or vCursor.x > (sCenterPos.x + C_HALF_SCALED_WIDTH)
+            or vCursor.y < (sCenterPos.y - C_HALF_SCALED_HEIGHT)
+            or vCursor.y > (sCenterPos.y + C_HALF_SCALED_HEIGHT)
+            then
+              outShouldDraw <= false;
+          else
+             vTranslatedCursor := (((vCursor.x - (sCenterPos.x - C_HALF_SCALED_WIDTH))  / SCALE), 
+                                   ((vCursor.y - (sCenterPos.y - C_HALF_SCALED_HEIGHT)) / SCALE));
+             -- for rotation, first we do a translation to have the origin in the center of the sprite
+             vTranslatedCursor := translateOriginToCenterOfSprite(SPRITE_SIZE, vTranslatedCursor);
+             -- then we apply the rotation
+             vTranslatedCursor := rotate(SPRITE_SIZE, vTranslatedCursor, sRotation);
+             -- and translate the origin back
+             vTranslatedCursor := translateOriginBackToFirstBitCorner(SPRITE_SIZE, vTranslatedCursor);
+             -- now we check the sprite content with the transformed cursor
+             if vTranslatedCursor.x < 0 or vTranslatedCursor.x > SPRITE_SIZE.width-1
+                or vTranslatedCursor.y <0 or vTranslatedCursor.y > SPRITE_SIZE.height-1 then
+                outShouldDraw <= false;
+             elsif sSpriteContent(vTranslatedCursor.y)(vTranslatedCursor.x) = '1' then
+                outShouldDraw <= true;
+             else
+                outShouldDraw <= false;
+             end if;
+          end if;
+      end if;
+  end process;
+end logic;
---
+++ unnamed_fpga_game/top_level_vga_test.vhd
@@ -0,0 +1,189 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.VgaUtils.all;
+use work.definitions.all;
+
+entity top_level_vga_test is
+  port (
+    clk   : in std_logic; -- Pin 23, 50MHz from the onboard oscilator.
+    rgb   : out std_logic_vector (2 downto 0); -- Pins 106, 105 and 104
+    hsync : out std_logic; -- Pin 101
+    vsync : out std_logic -- Pin 103
+  );
+end entity top_level_vga_test;
+
+architecture rtl of top_level_vga_test is
+  constant SQUARE_SIZE  : integer := 30; -- In pixels
+  constant SQUARE_SPEED : integer := 100_000;
+
+
+constant SCREEN_SIZE    : Size2D := (640,480);
+constant SCREEN_MARGINS : Pos2D  := (155,30);
+
+
+  -- VGA Clock - 25 MHz clock derived from the 50MHz built-in clock
+  signal vga_clk : std_logic;
+
+  signal rgb_input, rgb_output : std_logic_vector(2 downto 0);
+  signal rgb_square_color : std_logic_vector (2 downto 0) := COLOR_YELLOW;
+  signal vga_hsync, vga_vsync  : std_logic;
+  signal hpos, vpos            : integer;
+
+--  signal square_x           : integer range HDATA_BEGIN to HDATA_END := HDATA_BEGIN + H_HALF - SQUARE_SIZE/2;
+--  signal square_y           : integer range VDATA_BEGIN to VDATA_END := VDATA_BEGIN + V_HALF - SQUARE_SIZE/2;
+
+  signal cursorPosition      : Pos2D;
+  signal hPosVector, vPosVector : std_logic_vector(15 downto 0) := (others => '0');
+--  signal spritePosition      : Pos2D;
+
+  signal square_speed_count : integer range 0 to SQUARE_SPEED        := 0;
+  signal should_move_square : boolean;
+  signal should_draw_square1 : boolean;
+  signal should_draw_square2 : boolean;
+
+  -- nael
+  signal ticksForDynamicTextPositionUpdate : std_logic := '0';
+  signal xPosSprite, yPosSprite : integer := 0;
+  signal xPosNael, yPosNael : integer := 5;
+  signal xDirectionSprite, yDirectionSprite : boolean := true; 
+  signal xDirectionText, yDirectionText : boolean := true; 
+
+  signal sSmiley1Enabled : boolean := true;
+
+  component VgaController is
+    port (
+      clk     : in std_logic;
+      rgb_in  : in std_logic_vector (2 downto 0);
+      rgb_out : out std_logic_vector (2 downto 0);
+      hsync   : out std_logic;
+      vsync   : out std_logic;
+      hpos    : out integer;
+      vpos    : out integer
+    );
+  end component;
+begin
+
+cursorPosition <= (hpos - SCREEN_MARGINS.x, vpos - SCREEN_MARGINS.y);
+
+mySprite : entity work.sprite(logic)
+generic map(SCREEN_SIZE => SCREEN_SIZE,
+            INITIAL_POSITION => (200,200),
+            INITIAL_SPEED => (1, 1, 150000),
+            SPRITE_WIDTH => 11,
+            SCALE => 3,
+            SPRITE_CONTENT => "00011111000"
+                             &"00100000100"
+                             &"01000000010"
+                             &"10010001001"
+                             &"10000000001"
+                             &"10000100001"
+                             &"10100000101"
+                             &"10010001001"
+                             &"01001110010"
+                             &"00100000100"
+                             &"00011111000",
+            INITIAL_ROTATION_SPEED => (1, 4000000))
+port map (inClock       => vga_clk,
+          inEnabled     => true,
+          --inSpritePos   => spritePosition,
+          inCursorPos   => cursorPosition,
+          outShouldDraw => should_draw_square1,
+          inColision => should_draw_square2);
+
+  mySprite2 : entity work.sprite(logic)
+  generic map(SCREEN_SIZE => SCREEN_SIZE,
+              INITIAL_POSITION => (500,300),
+              INITIAL_SPEED => (1, -1, 180000),
+              SPRITE_WIDTH => 11,
+              SCALE => 7,
+                SPRITE_CONTENT => "00011111000"
+                                 &"00100000100"
+                                 &"01000000010"
+                                 &"10010001001"
+                                 &"10000100001"
+                                 &"10000000001"
+                                 &"10011111001"
+                                 &"10100000101"
+                                 &"01000000010"
+                                 &"00100000100"
+                                 &"00011111000",
+              INITIAL_ROTATION_SPEED => (-1, 8000000))
+  port map (inClock       => vga_clk,
+            inEnabled     => true,
+            --inSpritePos   => spritePosition,
+            inCursorPos   => cursorPosition,
+            outShouldDraw => should_draw_square2,
+            inColision => should_draw_square1);
+
+ -- mySprite2 : entity work.sprite(logic)
+ -- generic map(SPRITE_WIDTH => 11,
+ --             SCALE => 16,
+ --             SPRITE_CONTENT => "00011111000"
+ --                              &"00100000100"
+ --                              &"01000000010"
+ --                              &"10010001001"
+ --                              &"10000100001"
+ --                              &"10000000001"
+ --                              &"10011111001"
+ --                              &"10100000101"
+ --                              &"01000000010"
+ --                              &"00100000100"
+ --                              &"00011111000")
+ --            --  SPRITE_CONTENT => "00011111000"
+ --            --                   &"00100000100"
+ --            --                   &"01000000010"
+ --            --                   &"10010001001"
+ --            --                   &"10000100001"
+ --            --                   &"10000000001"
+ --            --                   &"10001110001"
+ --            --                   &"10010001001"
+ --            --                   &"01001110010"
+ --            --                   &"00100000100"
+ --            --                   &"00011111000")
+ -- port map (inClock       => vga_clk,
+ --           inEnabled     => not sSmiley1Enabled,
+ --           inSpritePos   => spritePosition,
+ --           inCursorPos   => cursorPosition,
+ --           outShouldDraw => should_draw_square2);
+--square_x <= xPosSprite;
+--square_y <= yPosSprite;
+
+  controller : VgaController port map(
+    clk     => vga_clk,
+    rgb_in  => rgb_input,
+    rgb_out => rgb_output,
+    hsync   => vga_hsync,
+    vsync   => vga_vsync,
+    hpos    => hpos,
+    vpos    => vpos
+  );
+
+  rgb   <= rgb_output;
+  hsync <= vga_hsync;
+  vsync <= vga_vsync;
+
+  -- We need 25MHz for the VGA so we divide the input clock by 2
+  process (clk)
+  begin
+    if (rising_edge(clk)) then
+      vga_clk <= not vga_clk;
+    end if;
+  end process;
+
+  process (vga_clk, should_draw_square1, should_draw_square2)
+  variable tempColorSum : std_logic_vector(2 downto 0) := "000";
+  begin
+    if rising_edge(vga_clk) then
+      tempColorSum := "000";
+      if should_draw_square1 then
+        tempColorSum := "010";
+      end if;
+      if should_draw_square2 then
+        tempColorSum := tempColorSum or "001";
+      end if;
+    end if;
+    rgb_input <= tempColorSum; 
+  end process;
+
+end architecture;
