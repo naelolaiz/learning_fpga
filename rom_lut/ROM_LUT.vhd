7e663b0706550a0c6844839f3f0637fa71c22876
nael <naelblog@gmail.com>
2022-08-14 20:12:48 +0200

    WIP

--- rom_lut/ROM_LUT.vhd
+++ rom_lut/ROM_LUT.vhd
@@ -13,11 +13,11 @@ ENTITY single_clock_rom IS
    GENERIC(
            ARRAY_SIZE          : integer := 32;
            ELEMENTS_BITS_COUNT : integer := 9
---           initFile            : string  := "SIN_TABLES_MULT_0_TO_HALF_PI_NORMALIZED_TO_UNSIGNED_9_BIT_ON_12_BIT_WORDS.hex"
    );
    PORT (
          clock: IN STD_LOGIC;
          read_address: IN INTEGER RANGE 0 to ARRAY_SIZE*16 - 1 ; -- we store hexadecimal numbers for a table for each index
+         --read_address: IN INTEGER RANGE 0 to ARRAY_SIZE*4 ; -- we store hexadecimal numbers for a table for each index
          output: OUT STD_LOGIC_VECTOR (ELEMENTS_BITS_COUNT-1 DOWNTO 0)
    );
 END single_clock_rom;
@@ -26,32 +26,6 @@ ARCHITECTURE rtl OF single_clock_rom IS
 
    type HexMultiplicationTableType is array (0 to 15) of std_logic_vector(ELEMENTS_BITS_COUNT-1 downto 0);
    type TableOfTablesType is array (0 to ARRAY_SIZE-1) of HexMultiplicationTableType;
-
-
- -- impure function initRomFromFile return TableOfTablesType is
- --    file data_file : text open read_mode is initFile;
- --    variable data_fileLine : line;
- --    variable ROM : TableOfTablesType;
- --    constant wordSizeCompleteNibbles : integer := natural(ceil(real(ELEMENTS_BITS_COUNT)/real(4)))*4;
- --    variable tempLineHexNumber : std_logic_vector (16*wordSizeCompleteNibbles - 1 downto 0); -- read by nibbles
- --    variable tempValueCompleteNibbles :  std_logic_vector (wordSizeCompleteNibbles-1 downto 0) := (others=>'0');
- -- begin
- -- -- read file with tables and fill in ROM
- --    for I in TableOfTablesType'range loop
- --       if(not endfile(data_file)) then
- --           readline(data_file, data_fileLine); 
- --           hread(data_fileLine, tempLineHexNumber); -- vhdl 2008
- --           for o in 0 to 15 loop
- --               tempValueCompleteNibbles := tempLineHexNumber((o+1)*wordSizeCompleteNibbles-1 downto o*wordSizeCompleteNibbles);
- --               ROM(I)(o) := tempValueCompleteNibbles (ELEMENTS_BITS_COUNT-1 downto 0);
- --           end loop;
- --       else
- --          ROM(I) := (others => (others => '1') ); -- ???????
- --       end if;
- --    end loop;
- --    return ROM;
- -- end function;
- --constant rom : TableOfTablesType := initRomFromFile;
 
 -- IMPORTANT!!!!!! In order to synthesize this as BRAM THIS NEEDS TO BE SIGNAL, NOT CONSTANT! Otherwise it will use logic elements instead of BRAM.
  signal rom : TableOfTablesType :=
