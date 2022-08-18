874df2f184bab491c7ea208173feda8f6c68e02c
nael <naelblog@gmail.com>
2022-08-18 23:43:36 +0200

    more

--- rom_lut/test/tb_rom_lut.vhd
+++ rom_lut/test/tb_rom_lut.vhd
@@ -6,16 +6,18 @@ entity tb_rom_lut is
 end tb_rom_lut;
 
 architecture testbench of tb_rom_lut is
-   signal sClock         : std_logic                    := '0';
-   signal sInAddress     : integer range 0 to 32*16-1   := 0;
-   signal sReadByte      : std_logic_vector(8 downto 0) := (others=>'0');
-   signal sTestRunning   : boolean                      := true;
-   constant CLOCK_PERIOD : time                         := 4 ns;
+   signal sClock            : std_logic                    := '0';
+   signal sAngleIdx         : std_logic_vector(6 downto 0) := (others => '0');
+   signal sNibbleProductIdx : std_logic_vector(3 downto 0) := (others => '0');
+   --signal sInAddress      : integer range 0 to 32*16-1   := 0;
+   signal sReadByte         : std_logic_vector(8 downto 0) := (others=>'0');
+   signal sTestRunning      : boolean                      := true;
+   constant CLOCK_PERIOD    : time                         := 4 ns;
   ---
-   signal sInAddressForSin : std_logic_vector (6 downto 0) := (others => '0');
-   signal sInFactor        : integer range -128 to 127     := 2;
-   signal sOutProduct      : integer range -256 to 255     := 0;
-   signal sOutDone         : std_logic                     := '0';
+   --signal sInAddressForSin  : std_logic_vector (6 downto 0) := (others => '0');
+   signal sInFactor         : integer range -128 to 127     := 2;
+   signal sOutProduct       : integer range -256 to 255     := 0;
+   signal sOutDone          : std_logic                     := '0';
 
 
 begin
@@ -24,14 +26,15 @@ sClock <= not sClock after CLOCK_PERIOD / 2 when sTestRunning;
 
 
 DUT : entity work.tl_rom_lut(logic)
-port map (inClock50Mhz => sClock,
-          inAddressToRead => sInAddress,
-          outReadMemory => sReadByte);
+port map (inClock50Mhz       => sClock,
+          inAngleIdxToRead   => sAngleIdx,
+          inNibbleProductIdx => sNibbleProductIdx,
+          outReadMemory      => sReadByte);
 
    read_all_addresses : process
    begin
-      for address in 0 to 32*16-1 loop
-         sInAddress <= address;
+      for address in 0 to 127 loop
+         sAngleIdx <= std_logic_vector(to_unsigned(address, 7));
          wait for CLOCK_PERIOD * 2;
          -- assert (to_integer(unsigned(sReadByte)) = 31 - address)
          --   report "wrong read value" severity error;
@@ -43,7 +46,7 @@ port map (inClock50Mhz => sClock,
 
  -- DUT2 : entity work.tl_sin_lut_reader(logic_sin_lut_reader)
  -- port map (inClock50Mhz    => sClock,
- --           inAddressToRead => sInAddressForSin,
+ --           inAngleIdxToRead => sInAddressForSin,
  --           inFactor        => sInFactor,
  --           outProduct      => sOutProduct,
  --           outDone         => sOutDone);
